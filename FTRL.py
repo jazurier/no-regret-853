b10ca77d14ea8cdcff96d8f873dd3c0c5ca2be99
Uma Roy <uma.roy.us@gmail.com>
2017-05-01 23:46:44 -0400

    Fuck you joe zurier i reimplemented your code

--- FTRL.py
+++ FTRL.py
@@ -1,132 +1,149 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize
-prisoner_dilemma_p1 = np.array([[-1,-3],[0,-2]])
-prisoner_dilemma_p2 = np.array([[-1,-3],[0,-2]])
-mixNE_ZS_p1 = np.array([[2,-1],[-1,0]])
-mixNE_ZS_p2 = -1*np.array([[2,-1],[-1,0]])
-battle_sexes_p1 = np.array([[2,0],[0,1]])
-battle_sexes_p2 = np.array([[1,0],[0,2]])
-staghunt_p1 = np.array([[2,0],[1,1]])
-staghunt_p2 = np.array([[2,1],[0,1]])
-
-matrix_p1 = staghunt_p1
-matrix_p2 = staghunt_p2
-# print mixNE_ZS_p2
-# matrix_p1 = prisoner_dilemma_p1
-# matrix_p2 = prisoner_dilemma_p2
-# matrix_p1 = mixNE_ZS_p1
-# matrix_p2 = mixNE_ZS_p2
-epochs = 2000
-tolerance = .001
-
-#eta_recip = np.sqrt(2*epochs*1./(1.-(1./2.)))
-
-def quad_regularizer(n_vector):
-	# nm = np.linalg.norm(n_vector)
-	nm = np.linalg.norm(n_vector-np.array([0.65,0]))
-	nm = nm**2
-	return 0.5*nm
-
-
-def L1_norm_constraint(x):
-	return sum(x)-1
-
-def starting_point():
-	p = np.random.rand()
-	return np.array([p,1-p])
-
-bounds = [(0,1) for _ in range(2)] #make this adaptive
-
-quad_argmin= scipy.optimize.fmin_slsqp(quad_regularizer, x0 = starting_point(), eqcons = [L1_norm_constraint], bounds = bounds)
-quad_min = quad_regularizer(quad_argmin)
-
-quad_argmax = scipy.optimize.fmin_slsqp(lambda x: -quad_regularizer(x), x0 = starting_point(), eqcons = [L1_norm_constraint], bounds = bounds)
-quad_max = quad_regularizer(quad_argmax)
-
-eta_recip = np.sqrt(epochs)/(quad_max-quad_min)
+import scipy
+from scipy import optimize
 
 class player():
-
-	def __init__(self, payoff_matrix):
-		self.payoff = payoff_matrix
+	def __init__(self, n_actions,regularizer,epochs,payoff_matrix=None,payoff_func=None):
+		self.n_actions = n_actions
+		self.regularizer = regularizer
+		self.eta_recip = calculate_eta(n_actions, regularizer,epochs)
+		print 'eta', self.eta_recip
+		self.payoff_matrix = payoff_matrix
+		self.payoff_func = payoff_func
+		if self.payoff_matrix is not None and self.payoff_func is None:
+			def mat_to_payoff(action, other_actions):
+				return np.dot(np.dot(action,payoff_matrix),other_actions[0]) + self.regularizer(action)*self.eta_recip
+			self.payoff_func = mat_to_payoff
 		self.action_history = []
 	def add_action(self,action):
 		self.action_history.append(action)
-	def get_dec(self):
+	def get_most_recent(self):
 		return self.action_history[-1]
-	def get_avg(self):
-		vec = self.action_history[0]*1.
-		vec = vec - self.action_history[0]
-		for vector in self.action_history:
-			vec += vector
-		return vec*1./(len(self.action_history))
+	def get_avg_action(self):
+		vec = np.zeros(self.n_actions)
+		for action in self.action_history:
+			vec += action
+		return vec/float(len(self.action_history))
 
-p1 = player(matrix_p1)
-p2 = player(matrix_p2)
+def quad_regularizer(n_vector):
+	nm = np.linalg.norm(n_vector)
+	nm = nm**2
+	return 0.5*nm
 
-def F_tilde(pl1,pl2,action,which_player_am_I):
-	cumsum = eta_recip*quad_regularizer(action)
-	for i in range(len(pl1.action_history)):
-		if which_player_am_I == 1:
-			cumsum += np.dot(np.dot(action,pl1.payoff),pl2.action_history[i])
-		elif which_player_am_I == 2:
-			cumsum += np.dot(np.dot(action,pl2.payoff),pl1.action_history[i])
-	return cumsum
+def simplex_sample(DIM):
+	samples = np.random.uniform(size=DIM+1) #this is sort of jank
+	samples[0]=0
+	samples[1] = 1
+	samples.sort()
+	prob = []
+	for i in range(1,DIM+1):
+		prob.append(samples[i]-samples[i-1])
+	assert(sum(prob) == 1.)
+	return prob
 
-def argmax_function(first_player,second_player,which_player_am_I,t):
-	def opt(x):
-		return -F_tilde(first_player,second_player,x,which_player_am_I)
-	
+def calculate_eta(DIM, regularizer, epochs):
+	#eta_recip = np.sqrt(2*epochs*1./(1.-(1./2.)))
 	def L1_norm_constraint(x):
 		return sum(x)-1
 
-	bounds = [(0,1) for _ in range(2)] #make this adaptive
-	p = np.random.rand()
-	starting_point = np.array([p,1-p])
-	amin = scipy.optimize.fmin_slsqp(opt, x0 = starting_point, eqcons = [L1_norm_constraint], bounds = bounds,iprint=0)
-	# if t==0:
-	# 	print amin,"TIME ZERO"
-	amin = amin.clip(min=0)
-	amin = amin/sum(amin)
-	#assert(sum(amin)==1)
-	newact = np.random.choice(2, p=amin)
-	act = np.zeros(2)
-	act[newact] = 1
-	#assert(sum(act) == 1)
-	# print act,amin
-	# print opt(amin)
-	# print opt([0.25,0.75])
-	return act
-	# return amin
+	starting_point = simplex_sample(DIM)
+	#np.asarray([1./DIM for _ in range(DIM)])
 
-	# return the action * that minimizes F_tilde(pl1,pl2,*,which_player_am_I)
-def update_FTRL(pl1,pl2,t):
-	newp1act = argmax_function(pl1,pl2,1,t)
-	newp2act = argmax_function(pl1,pl2,2,t)
-	pl1.add_action(newp1act)
-	pl2.add_action(newp2act)
-	return pl1,pl2
+	bounds = [(0,1) for _ in range(DIM)] #make this adaptive
 
-def find_Nash(p1,p2):
-	t=0
-	k=0
-	for i in range(k):
-		# p1.add_action(np.array([2./3,1./3]))
-		# p2.add_action(np.array([1./3,2./3]))
-		p1.add_action(np.array([0,1]))
-		p2.add_action(np.array([0,1]))
-		t+=1
-	while t<epochs:
-		if t%50 == 0:
-			print 'TIME '
-			print t
-			# print 'TIME '
-			# print t
-			# print 'TIME '
-			# print t
-		p1,p2 = update_FTRL(p1,p2,t)
-		t+=1
-	# print p1.action_history
-	return p1.get_dec(),p2.get_dec(),'AVG',p1.get_avg(),p2.get_avg()
-print find_Nash(p1,p2)
+	argmin= scipy.optimize.fmin_slsqp(regularizer, x0 = starting_point, eqcons = [L1_norm_constraint], bounds = bounds,iprint=0)
+	rmin = regularizer(argmin)
+
+	argmax = scipy.optimize.fmin_slsqp(lambda x: -regularizer(x), x0 = starting_point, eqcons = [L1_norm_constraint], bounds = bounds,iprint=0)
+	rmax = regularizer(argmax)
+
+	# print 'argmin, argmix', argmin, argmax
+	eta_recip = np.sqrt(epochs)/(rmax-rmin)
+	return eta_recip
+
+# def F_tilde(pl1,pl2,action,which_player_am_I):
+# 	cumsum = eta_recip*quad_regularizer(action)
+# 	for i in range(len(pl1.action_history)):
+# 		if which_player_am_I == 1:
+# 			cumsum += np.dot(np.dot(action,pl1.payoff),pl2.action_history[i])
+# 		elif which_player_am_I == 2:
+# 			cumsum += np.dot(np.dot(action,pl2.payoff),pl1.action_history[i])
+# 	return cumsum
+
+def F_tilde(all_players, player, action):
+	s = player.eta_recip*player.regularizer(action)
+	for i in range(len(player.action_history)): 
+		other_actions = []
+		for p in all_players:
+			if p is player:
+				continue
+			other_actions.append(p.action_history[i]) #list of action histories
+		s += player.payoff_func(action, other_actions)
+	return s
+
+def find_opt_distribution(players, p):
+	DIM = p.n_actions
+
+	bounds = [(0,1) for _ in range(DIM)]
+	def L1_norm_constraint(x):
+		return sum(x)-1
+	starting_point = [1./DIM for _ in range(DIM)] #maybe it's better to sample from DIM-dimensional simplex
+
+	def cost(action): #return negative since scipy.optimze finds argmin
+		return -F_tilde(players,p,action)
+
+	opt_dist = scipy.optimize.fmin_slsqp(cost,x0=starting_point,eqcons=[L1_norm_constraint],bounds=bounds,iprint=0)
+	opt_dist = opt_dist.clip(min=0)
+	opt_dist = opt_dist/sum(opt_dist)
+	return opt_dist
+
+def update_FTRL(players):
+	for p in players:
+		opt_dist = find_opt_distribution(players, p)
+		action = np.random.choice(len(opt_dist),p=opt_dist)
+		action_vec = np.zeros(len(opt_dist))
+		action_vec[action] = 1
+		p.add_action(action_vec)
+
+if __name__ == '__main__':
+	epochs = 200
+
+	prisoner_dilemma_p1 = np.array([[-1,-3],[0,-2]])
+	prisoner_dilemma_p2 = np.array([[-1,-3],[0,-2]])
+	mixNE_ZS_p1 = np.array([[2,-1],[-1,0]])
+	mixNE_ZS_p2 = -1*np.array([[2,-1],[-1,0]])
+	battle_sexes_p1 = np.array([[2,0],[0,1]])
+	battle_sexes_p2 = np.array([[1,0],[0,2]])
+	staghunt_p1 = np.array([[2,0],[1,1]])
+	staghunt_p2 = np.array([[2,1],[0,1]])
+
+	matrix_p1 = mixNE_ZS_p1
+	matrix_p2 = mixNE_ZS_p2
+
+	#player1 = player(n_actions = 2, payoff_matrix = matrix_p1,regularizer=quad_regularizer,epochs=epochs)
+	#player2 = player(n_actions = 2, payoff_matrix = matrix_p2,regularizer=quad_regularizer,epochs=epochs)
+	#players = [player1, player2]
+
+	N_players = 4
+	def payoff_func(action,other_actions): #[0=not call,1=call]
+		#Pr no one calls = 
+		pr_no_call = 1
+		for ac in other_actions:
+			pr_no_call *= ac[0]
+
+		pr_call = 1-pr_no_call
+		util = action[1] + action[0]*(pr_no_call*0 + pr_call*2)
+		return util
+
+	players = []
+	for _ in range(N_players):
+		new_player = player(n_actions=2,payoff_func=payoff_func,regularizer=quad_regularizer,epochs=epochs)
+		players.append(new_player)
+
+	for i in range(epochs):
+		if i % 20 == 0:
+			print 'iteration',  i
+		update_FTRL(players)
+	for p in players:
+		print p.get_avg_action()
